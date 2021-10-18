dd8b887802bbe7aa1f88576e0b317db4074efc88
Rafael Estrada <rafaelestrada@comcast.net>
2021-10-17 21:17:30 -0700

    Initial project

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = (props) => (
+ <Section row nopadding>
+   <LeftSection>
+     <SectionTitle main center>
+       Welcome To <br/>
+       My Personal Portfolio
+     </SectionTitle>
+     <SectionText>
+       Dedicated to learning, growing and succeeding in computers software.  I 
+       offer my best professional ability to become part of team.
+     </SectionText>
+     <Button onClick={()=> window.location='https://google.com'}>Learn More</Button>
+
+   </LeftSection>
+ </Section>
+);
+
+export default Hero;
